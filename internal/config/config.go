43815b530ef130c0f391b06fc24ece66681263b1
egorolkhov <egor.olhov@gmail.com>
2023-11-14 06:46:19 +0300

    iter10

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,22 +1,23 @@
 package config
 
 import (
+	"errors"
 	"flag"
 	"github.com/egorolkhov/shortener/internal/config/netAddress"
-	"github.com/egorolkhov/shortener/internal/config/postgresqladdress"
 	"os"
+	"strings"
 )
 
 type Cfg struct {
 	Address     netAddress.NetAddress
 	BaseURL     string
 	Filepath    string
-	DatabaseDSN postgresqladdress.PGXaddress
+	DatabaseDSN PGXaddress
 }
 
 func Config() *Cfg {
 	address := netAddress.NewNetAddress()
-	databaseDSN := postgresqladdress.NewPGXaddress()
+	databaseDSN := NewPGXaddress()
 	baseURL := ""
 
 	flag.Var(address, "a", "http server adress")
@@ -38,4 +39,56 @@ func Config() *Cfg {
 	_ = databaseDSN.Set(os.Getenv("DATABASE_DSN"))
 
 	return &Cfg{Address: *address, BaseURL: *url, Filepath: *filepath, DatabaseDSN: *databaseDSN}
+}
+
+type PGXaddress struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBname   string
+	SSLmode  string
+}
+
+func (n *PGXaddress) String() string {
+	sb := strings.Builder{}
+
+	sb.WriteString(n.Host)
+	sb.WriteString(":")
+	sb.WriteString(n.Port)
+	sb.WriteString(":")
+	sb.WriteString(n.User)
+	sb.WriteString(":")
+	sb.WriteString(n.Password)
+	sb.WriteString(":")
+	sb.WriteString(n.DBname)
+	sb.WriteString(":")
+	sb.WriteString(n.SSLmode)
+
+	return sb.String()
+}
+
+func (n *PGXaddress) Set(flagValue string) error {
+	res := strings.Split(flagValue, ":")
+	if len(res) != 6 {
+		return errors.New("wrong address")
+	}
+	n.Host = res[0]
+	n.Port = res[1]
+	n.User = res[2]
+	n.Password = res[3]
+	n.DBname = res[4]
+	n.SSLmode = res[5]
+	return nil
+}
+
+func NewPGXaddress() *PGXaddress {
+	return &PGXaddress{
+		Host:     "localhost",
+		Port:     "5432",
+		User:     "shortener",
+		Password: "Qazxsw2200",
+		DBname:   "shortener",
+		SSLmode:  "disable",
+	}
 }
